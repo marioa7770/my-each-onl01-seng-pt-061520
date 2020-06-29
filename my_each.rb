0c83c2f22bae9277ad8f3050f3f78c8cb9a4dede
mario acevedo <jun_acevedo415@yahoo.com>
2020-06-29 02:10:35 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
