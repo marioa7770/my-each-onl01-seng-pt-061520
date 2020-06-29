043b0e376ba1f95d184dddbcb7146b61887de429
mario acevedo <jun_acevedo415@yahoo.com>
2020-06-29 02:16:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,8 @@
 def my_each(array)
   i = 0
   while i < array.length
-# The "while" method will always return a value of => nil"
     yield(array[i])
-    i = i + 1
+    i += 1
   end
+  array
 end
-
-my_each(list) {|i| puts "So I think #{i} is a piece of shit"}
